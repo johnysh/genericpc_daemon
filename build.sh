ebbaffcb3ce7daae31137f2246a301a0a3524b5d
johnysh <junhan.shi@intel.com>
2023-11-22 11:02:27 +0800

    add code for generate genericpc daemon deb

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# GenericPC Power Daemon
+# Building GenericPC Power  Daemon script
+
+WORKDIR=$PWD
+
+mkdir -p ${WORKDIR}/debian/usr/lib/systemd/system
+mkdir -p ${WORKDIR}/debian/usr/sbin
+
+mkdir -p ${WORKDIR}/debian/var/cache/gpowerd
+chmod 600 ${WORKDIR}/debian/var/cache/gpowerd
+
+
+#VER=$(grep VERSION Makefile | awk 'NR==5{print $2}' FS=" = ")
+VER=1.0.0
+sed -i "s/Version:.*/Version: ${VER}/g" ${WORKDIR}/debian/DEBIAN/control
+
+cp -f  generic_pc_powerd ${WORKDIR}/debian/usr/sbin
+chmod 755 ${WORKDIR}/debian/usr/sbin/generic_pc_powerd
+#cd ../..
+
+cp -f  genericpc.service ${WORKDIR}/debian/usr/lib/systemd/system
+
+cp -f saved_parameters.genericpcd ${WORKDIR}/debian/var/cache/gpowerd
+cp -f saved_results.genericpcd ${WORKDIR}/debian/var/cache/gpowerd 
+
+
+rm -f *.deb
+
+dpkg-deb --build --root-owner-group debian
+dpkg-name debian.deb
